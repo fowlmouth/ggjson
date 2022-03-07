8ac0e9080195802448bf4c187034cd89f3994df8
fowl <phowl.mouth@gmail.com>
2022-03-07 17:01:40 -0600

    update lexer tests to use test state for memory management

--- test/lexer.c
+++ test/lexer.c
@@ -4,6 +4,30 @@
 
 #include <string.h>
 
+
+
+struct lexer_test_state
+{
+  ggjson_lexer_token token;
+};
+
+void lexer_test_setup(void* arg)
+{
+  struct lexer_test_state* state = arg;
+  ggjson_lexer_token_init(&state->token, 256);
+}
+
+void lexer_test_destroy(void* arg)
+{
+  struct lexer_test_state* state = arg;
+  ggjson_lexer_token_deinit(&state->token);
+}
+
+SUITE(lexer, sizeof(struct lexer_test_state), lexer_test_setup, lexer_test_destroy)
+
+
+
+
 TEST(lexer, tokens)
 {
   const char* string = "[ 10, 20, 30 ]";
@@ -12,26 +36,26 @@ TEST(lexer, tokens)
   const int error_buffer_size = 256;
   char error_buffer[error_buffer_size];
 
+  struct lexer_test_state* state = data;
   ggjson_string_input string_input;
   ggjson_string_input_init(&string_input, string, strlen(string));
  
   ggjson_lexer lexer;
   ggjson_lexer_init(&lexer, (ggjson_input*)&string_input);
 
-  ggjson_lexer_token token __attribute__((cleanup(ggjson_lexer_token_deinit)));
-  ggjson_lexer_token_init(&token, 256);
+  ggjson_lexer_token *token = &state->token;
 
   for(int i = 0; i < token_count; ++i)
   {
     ASSERT_MSGF(
-      ggjson_lexer_read_token(&lexer, &token, error_buffer_size, error_buffer),
+      ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer),
       "expected a token %d/%d", i, token_count);
   }
 
   ASSERT_MSG(
-    ggjson_lexer_read_token(&lexer, &token, error_buffer_size, error_buffer),
+    ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer),
     "expected a token (EOF)");
-  ASSERT_EQ(token.type, ggjltt_eof);
+  ASSERT_EQ(token->type, ggjltt_eof);
 
   PASS();
 }
@@ -43,24 +67,54 @@ TEST(lexer, integers)
   const int error_buffer_size = 256;
   char error_buffer[error_buffer_size];
 
+  struct lexer_test_state* state = data;
   ggjson_string_input string_input;
   ggjson_string_input_init(&string_input, string, strlen(string));
  
   ggjson_lexer lexer;
   ggjson_lexer_init(&lexer, (ggjson_input*)&string_input);
 
-  ggjson_lexer_token token __attribute__((cleanup(ggjson_lexer_token_deinit)));
-  ggjson_lexer_token_init(&token, 256);
+  ggjson_lexer_token *token = &state->token;
 
-  ASSERT(ggjson_lexer_read_token(&lexer, &token, error_buffer_size, error_buffer));
-  ASSERT_EQ(token.int_value, 98765);
+  ASSERT(ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer));
+  ASSERT_EQ(token->type, ggjltt_integer);
+  ASSERT_EQ(token->int_value, 98765);
 
-  ASSERT(ggjson_lexer_read_token(&lexer, &token, error_buffer_size, error_buffer));
-  ASSERT_EQ(token.int_value, -43210);
+  ASSERT(ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer));
+  ASSERT_EQ(token->type, ggjltt_integer);
+  ASSERT_EQ(token->int_value, -43210);
 
-  ASSERT_EQ(ggjlrtr_eof, ggjson_lexer_read_token(&lexer, &token, error_buffer_size, error_buffer));
-  ASSERT_EQ(token.type, ggjltt_eof);
+  ASSERT_EQ(ggjlrtr_eof, ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer));
+  ASSERT_EQ(token->type, ggjltt_eof);
 
   PASS();
 }
 
+TEST(lexer, punctuation)
+{
+  const char* string = "[]{},:";
+  const int token_count = 6;
+
+  const int error_buffer_size = 256;
+  char error_buffer[error_buffer_size];
+
+  struct lexer_test_state* state = data;
+  ggjson_string_input string_input;
+  ggjson_string_input_init(&string_input, string, strlen(string));
+ 
+  ggjson_lexer lexer;
+  ggjson_lexer_init(&lexer, (ggjson_input*)&string_input);
+
+  ggjson_lexer_token *token = &state->token;
+
+  for(int i = 0; i < token_count; ++i)
+  {
+    ASSERT(ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer));
+  }
+
+  ASSERT(ggjson_lexer_read_token(&lexer, token, error_buffer_size, error_buffer));
+  ASSERT_EQ(token->type, ggjltt_eof);
+
+  PASS();
+
+}
