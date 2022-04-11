67af16e7c95faea8b7766f8b7c519ee230efcefa
fowl <phowl.mouth@gmail.com>
2022-04-10 20:25:37 -0500

    clean up parser tests a bit

--- test/parser.c
+++ test/parser.c
@@ -25,12 +25,14 @@ SUITE(parser, sizeof(struct parser_test_state), parser_test_setup, parser_test_d
 
 
 
+#define PARSER_TEST_DATA_BUFFER_SIZE 64
 
 struct parser_test_data
 {
   int int_count, true_count, false_count, null_count;
   long long integer_value;
   double double_value;
+  char string_buffer[PARSER_TEST_DATA_BUFFER_SIZE];
 };
 
 int test_accept_integer(ggjson_parser_state* unused, void* arg, const char* key, long long value)
@@ -42,115 +44,19 @@ int test_accept_integer(ggjson_parser_state* unused, void* arg, const char* key,
   return 1;
 }
 
-TEST(parser, integer)
-{
-  const char* string = "12345 -99 42";
-
-  ggjson_string_input string_input;
-  ggjson_string_input_init(&string_input, string, strlen(string));
-
-  ggjson_parser_events events;
-  ggjson_parser_events_init(&events);
-  events.accept_integer = test_accept_integer;
-
-  ggjson_parser* parser = &((struct parser_test_state*)data)->parser;
-  ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
-
-  struct parser_test_data ptd;
-
-  ASSERT(ggjson_parse_node(parser, &ptd));
-  ASSERT_EQ(ptd.int_count, 1);
-  ASSERT_EQ(ptd.integer_value, 12345);
-  memset(&ptd, 0, sizeof ptd);
-
-  ASSERT(ggjson_parse_node(parser, &ptd));
-  ASSERT_EQ(ptd.int_count, 1);
-  ASSERT_EQ(ptd.integer_value, -99);
-  memset(&ptd, 0, sizeof ptd);
-
-  ASSERT(ggjson_parse_node(parser, &ptd));
-  ASSERT_EQ(ptd.int_count, 1);
-  ASSERT_EQ(ptd.integer_value, 42);
-  memset(&ptd, 0, sizeof ptd);
-
-  ASSERT(ggjson_input_is_eof((ggjson_input*)&string_input));
-  ASSERT(!ggjson_parse_node(parser, &ptd));
-}
-
-struct string_buffer
-{
-  int size;
-  char* buffer;
-};
-
 int test_accept_string(ggjson_parser_state* unused, void* arg, const char* key, int size, const char* str)
 {
-  struct string_buffer* output = arg;
-  snprintf(output->buffer, output->size, "%s", str);
+  struct parser_test_data* output = arg;
+  snprintf(output->string_buffer, PARSER_TEST_DATA_BUFFER_SIZE, "%s", str);
   return 1;
-}
-
-TEST(parser, string)
-{
-  const char* string = "\"A🍄B🌶C🍍\" \"foo\nbar\"";
-
-  ggjson_string_input string_input;
-  ggjson_string_input_init(&string_input, string, strlen(string));
-
-  ggjson_parser_events events;
-  ggjson_parser_events_init(&events);
-  events.accept_string = test_accept_string;
-
-  ggjson_parser* parser = &((struct parser_test_state*)data)->parser;
-  ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
-
-  char output[64];
-  struct string_buffer strbuf = {64, output};
-
-  ASSERT(ggjson_parse_node(parser, &strbuf));
-  ASSERT_EQ(0, strcmp(output, "A🍄B🌶C🍍"));
-
-  memset(output, 0, sizeof output);
-
-  ASSERT(ggjson_parse_node(parser, &strbuf));
-  ASSERT_EQ(0, strcmp(output, "foo\nbar"));
-
-  ASSERT(!ggjson_parse_node(parser, &strbuf));
 }
 
 int test_accept_double(ggjson_parser_state* unused, void* arg, const char* key, double value)
 {
-  double* output = arg;
-  *output = value;
+  struct parser_test_data* output = arg;
+  output->double_value = value;
   return 1;
 }
-
-TEST(parser, double)
-{
-  const char* string = "123.45 -99e2";
-
-  ggjson_string_input string_input;
-  ggjson_string_input_init(&string_input, string, strlen(string));
-
-  ggjson_parser_events events;
-  ggjson_parser_events_init(&events);
-  events.accept_double = test_accept_double;
-
-  ggjson_parser* parser = &((struct parser_test_state*)data)->parser;
-  ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
-
-  double value = 0;
-
-  ASSERT(ggjson_parse_node(parser, &value));
-  ASSERT_EQ(value, 123.45);
-
-  ASSERT(ggjson_parse_node(parser, &value));
-  ASSERT_EQ(value, -99e2);
-
-}
-
-
-
 
 int test_accept_true(ggjson_parser_state* unused, void* arg, const char* key)
 {
@@ -171,6 +77,113 @@ int test_accept_null(ggjson_parser_state* unused, void* arg, const char* key)
   return 1;
 }
 
+
+void init_test_events(ggjson_parser_events* ev)
+{
+  ggjson_parser_events_init(ev);
+  ev->accept_integer = test_accept_integer;
+  ev->accept_string  = test_accept_string;
+  ev->accept_double  = test_accept_double;
+  ev->accept_true    = test_accept_true;
+  ev->accept_false   = test_accept_false;
+  ev->accept_null    = test_accept_null;
+}
+
+
+
+TEST(parser, integer)
+{
+  const char* string = "12345 -99 42";
+
+  ggjson_string_input string_input;
+  ggjson_string_input_init(&string_input, string, strlen(string));
+
+  ggjson_parser_events events;
+  init_test_events(&events);
+
+  struct parser_test_state* test_state = data;
+  ggjson_parser* parser = &test_state->parser;
+  ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
+
+  struct parser_test_data ptd;
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(ptd.int_count, 1);
+  ASSERT_EQ(ptd.integer_value, 12345);
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(ptd.int_count, 1);
+  ASSERT_EQ(ptd.integer_value, -99);
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(ptd.int_count, 1);
+  ASSERT_EQ(ptd.integer_value, 42);
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_input_is_eof((ggjson_input*)&string_input));
+  ASSERT(!ggjson_parse_node(parser, &ptd));
+}
+
+TEST(parser, string)
+{
+  const char* string = "\"A🍄B🌶C🍍\" \"foo\nbar\"";
+
+  ggjson_string_input string_input;
+  ggjson_string_input_init(&string_input, string, strlen(string));
+
+  ggjson_parser_events events;
+  init_test_events(&events);
+
+  struct parser_test_state* test_state = data;
+  ggjson_parser* parser = &test_state->parser;
+  ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
+
+  struct parser_test_data ptd;
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(0, strcmp(ptd.string_buffer, "A🍄B🌶C🍍"));
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(0, strcmp(ptd.string_buffer, "foo\nbar"));
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(!ggjson_parse_node(parser, &ptd));
+}
+
+TEST(parser, double)
+{
+  const char* string = "123.45 -99e2";
+
+  ggjson_string_input string_input;
+  ggjson_string_input_init(&string_input, string, strlen(string));
+
+  ggjson_parser_events events;
+  init_test_events(&events);
+
+  struct parser_test_state* test_state = data;
+  ggjson_parser* parser = &test_state->parser;
+  ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
+
+  struct parser_test_data ptd;
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(ptd.double_value, 123.45);
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(ggjson_parse_node(parser, &ptd));
+  ASSERT_EQ(ptd.double_value, -99e2);
+  memset(&ptd, 0, sizeof ptd);
+
+  ASSERT(!ggjson_parse_node(parser, &ptd));
+}
+
+
 TEST(parser, literals)
 {
   const char* string = "true false null";
@@ -179,12 +192,10 @@ TEST(parser, literals)
   ggjson_string_input_init(&string_input, string, strlen(string));
 
   ggjson_parser_events events;
-  ggjson_parser_events_init(&events);
-  events.accept_true  = test_accept_true;
-  events.accept_false = test_accept_false;
-  events.accept_null  = test_accept_null;
+  init_test_events(&events);
 
-  ggjson_parser* parser = &((struct parser_test_state*)data)->parser;
+  struct parser_test_state* test_state = data;
+  ggjson_parser* parser = &test_state->parser;
   ggjson_parser_init(parser, &events, (ggjson_input*)&string_input);
 
   struct parser_test_data ptd;
